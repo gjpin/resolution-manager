1bd30df743b691a1aa716f732df9a34eadf7e27f
gjpin <3874515+gjpin@users.noreply.github.com>
2024-09-11 17:36:59 +0100

    change resolution/frequency of external monitor only

--- main.go
+++ main.go
@@ -19,9 +19,6 @@ const (
 	DISP_CHANGE_BADMODE    uint32 = 0xFFFFFFFE
 )
 
-// DEVMODE is a structure used to
-// specify characteristics of display
-// and print devices.
 type DEVMODE struct {
 	DmDeviceName       [CCHDEVICENAME]uint16
 	DmSpecVersion      uint16
@@ -67,45 +64,92 @@ func main() {
 
 	// Load the DLL and the procedures.
 	user32dll := syscall.NewLazyDLL("user32.dll")
+	procEnumDisplayDevicesW := user32dll.NewProc("EnumDisplayDevicesW")
 	procEnumDisplaySettingsW := user32dll.NewProc("EnumDisplaySettingsW")
-	procChangeDisplaySettingsW := user32dll.NewProc("ChangeDisplaySettingsW")
+	procChangeDisplaySettingsExW := user32dll.NewProc("ChangeDisplaySettingsExW")
 
-	// Get the display information.
-	devMode := new(DEVMODE)
-	ret, _, _ := procEnumDisplaySettingsW.Call(uintptr(unsafe.Pointer(nil)),
-		uintptr(ENUM_CURRENT_SETTINGS), uintptr(unsafe.Pointer(devMode)))
+	// Find the secondary monitor
+	var dd DISPLAY_DEVICE
+	dd.Cb = uint32(unsafe.Sizeof(dd))
+	var i uint32 = 0
+	var secondaryMonitorName *uint16
 
-	if ret == 0 {
-		fmt.Println("Couldn't extract display settings.")
-		os.Exit(1)
+	for {
+		ret, _, _ := procEnumDisplayDevicesW.Call(0, uintptr(i), uintptr(unsafe.Pointer(&dd)), 0)
+		if ret == 0 {
+			break
+		}
+		if dd.StateFlags&DISPLAY_DEVICE_ATTACHED_TO_DESKTOP != 0 && dd.StateFlags&DISPLAY_DEVICE_PRIMARY_DEVICE == 0 {
+			secondaryMonitorName = &dd.DeviceName[0]
+			break
+		}
+		i++
 	}
 
-	// Show the display information.
-	fmt.Printf("Screen resolution: %dx%d\n", devMode.DmPelsWidth, devMode.DmPelsHeight)
+	if secondaryMonitorName == nil {
+		fmt.Println("No secondary monitor found.")
+		return
+	}
+
+	// Get the display information for the secondary monitor
+	devMode := new(DEVMODE)
+	devMode.DmSize = uint16(unsafe.Sizeof(*devMode))
+	ret, _, _ := procEnumDisplaySettingsW.Call(uintptr(unsafe.Pointer(secondaryMonitorName)),
+		uintptr(ENUM_CURRENT_SETTINGS), uintptr(unsafe.Pointer(devMode)))
+	if ret == 0 {
+		fmt.Println("Couldn't extract display settings for the secondary monitor.")
+		return
+	}
+
+	// Show the current display information for the secondary monitor
+	fmt.Printf("Secondary monitor resolution: %dx%d\n", devMode.DmPelsWidth, devMode.DmPelsHeight)
 	fmt.Printf("Bits per pixel: %d\n", devMode.DmBitsPerPel)
-	fmt.Printf("Display orientation: %d\n", devMode.DmOrientation)
-	fmt.Printf("Display flags: %x\n", devMode.DmDisplayFlags)
 	fmt.Printf("Display frequency: %d\n", devMode.DmDisplayFrequency)
 
-	// Change the display resolution.
+	// Change the display resolution for the secondary monitor
 	newMode := *devMode
 	newMode.DmPelsWidth = uint32(argWidth)
 	newMode.DmPelsHeight = uint32(argHeight)
 	newMode.DmDisplayFrequency = uint32(argFrequency)
-	ret, _, _ = procChangeDisplaySettingsW.Call(uintptr(unsafe.Pointer(&newMode)),
-		uintptr(0))
+	newMode.DmFields = DM_PELSWIDTH | DM_PELSHEIGHT | DM_DISPLAYFREQUENCY
+
+	ret, _, _ = procChangeDisplaySettingsExW.Call(
+		uintptr(unsafe.Pointer(secondaryMonitorName)),
+		uintptr(unsafe.Pointer(&newMode)),
+		0,
+		CDS_UPDATEREGISTRY|CDS_NORESET,
+		0)
 
 	switch ret {
 	case uintptr(DISP_CHANGE_SUCCESSFUL):
-		fmt.Println("Successfuly changed the display resolution and frequency.")
-
+		fmt.Println("Successfully changed the secondary monitor resolution and frequency.")
 	case uintptr(DISP_CHANGE_RESTART):
 		fmt.Println("Restart required to apply the resolution changes.")
-
 	case uintptr(DISP_CHANGE_BADMODE):
-		fmt.Println("The resolution or frequency are not supported by the display.")
-
+		fmt.Println("The resolution or frequency are not supported by the secondary monitor.")
 	case uintptr(DISP_CHANGE_FAILED):
-		fmt.Println("Failed to change the display resolution and frequency.")
+		fmt.Println("Failed to change the secondary monitor resolution and frequency.")
 	}
+
+	// Apply the changes
+	procChangeDisplaySettingsExW.Call(0, 0, 0, 0, 0)
 }
+
+type DISPLAY_DEVICE struct {
+	Cb           uint32
+	DeviceName   [32]uint16
+	DeviceString [128]uint16
+	StateFlags   uint32
+	DeviceID     [128]uint16
+	DeviceKey    [128]uint16
+}
+
+const (
+	DISPLAY_DEVICE_ATTACHED_TO_DESKTOP = 0x00000001
+	DISPLAY_DEVICE_PRIMARY_DEVICE      = 0x00000004
+	DM_PELSWIDTH                       = 0x00080000
+	DM_PELSHEIGHT                      = 0x00100000
+	DM_DISPLAYFREQUENCY                = 0x00400000
+	CDS_UPDATEREGISTRY                 = 0x00000001
+	CDS_NORESET                        = 0x10000000
+)
